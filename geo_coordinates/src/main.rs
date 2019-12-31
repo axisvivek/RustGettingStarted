3d58d7c6f0bec36f7e567eb853a9801a2d2048cf
Vivek Kumar <axis.vivekk@gmail.com>
2019-12-31 16:01:52 +0530

    Sample2: Color hex code

--- geo_coordinates/src/main.rs
+++ geo_coordinates/src/main.rs
@@ -23,16 +23,16 @@ impl Display for City {
 
 #[derive(Debug)]
 struct Color {
-    red: u8,
-    green: u8,
-    blue: u8,
+    red: u64,
+    green: u64,
+    blue: u64,
 }
 
 impl Display for Color {
     // `f` is a buffer, and this method must write the formatted string into it
-    fn fmt(&self, f: &mut Formatter) -> fmt::Result {       
-        write!(f, "RGB ({},{},{} {:X})",
-               self.red, self.green,self.blue,number)
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {  
+        write!(f, "RGB ({},{},{}) 0x{:02X}{:02X}{:02X}",
+               self.red, self.green,self.blue,self.red, self.green,self.blue)
                
     }
 }
@@ -54,5 +54,29 @@ fn main() {
         // for fmt::Display.
         println!("{}", *color);
     }
+
+    // Variables can be type annotated.
+    //let logical: bool = true;
+
+    //let a_float: f64 = 1.0;  // Regular annotation
+    //let an_integer   = 5i32; // Suffix annotation
+
+    // Or a default will be used.
+    //let default_float   = 3.0; // `f64`
+    //let default_integer = 7;   // `i32`
+    
+    // A type can also be inferred from context 
+    //let mut inferred_type = 12; // Type i64 is inferred from another line
+    //inferred_type = 4294967296i64;
+    
+    // A mutable variable's value can be changed.
+    //let mut mutable = 12; // Mutable `i32`
+    //mutable = 21;
+    
+    // Error! The type of a variable can't be changed.
+    //mutable = true;
+    
+    // Variables can be overwritten with shadowing.
+    //let mutable = true;
 }
 
