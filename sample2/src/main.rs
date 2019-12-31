c0b515e9f4fca5d4cf8b32afe016177497430d60
Vivek Kumar <axis.vivekk@gmail.com>
2019-12-31 12:24:28 +0530

    Sample2: Print local and UTC time

--- sample2/src/main.rs
+++ sample2/src/main.rs
@@ -1,16 +1,42 @@
-use std::io;
-use std::fs;
+extern crate chrono;
+use chrono::{DateTime, Utc,Local, TimeZone};
+
+// This is the main function
+fn main() {
+    // Statements here are executed when the compiled binary is called
+
+    // Welcome to Axis
+    println!("Hello Axis!");
+ /*
+======================================================
+ACAP Development using RUST : Vivek Kumar (31-12-2019)
+======================================================
+
+ */
+    println!("You can build ACAP Application using Rust :)");
+
+    print!("31-");
+    print!("12-");
+    print!("2019");
+    println!();
+    //println!("{}",now.to_string());
+
+    let now: DateTime<Utc> = Utc::now();
+    let loc = chrono::Local::now();
+
+    println!("UTC now is: {}", now);
+    println!("UTC now in RFC 2822 is: {}", now.to_rfc2822());
+    println!("UTC now in RFC 3339 is: {}", now.to_rfc3339());
+    println!("UTC now in a custom format is: {}", now.format("%a %b %e %T %Y"));
+
+    
+    println!("{:?}", loc);
+
+    println!("{:?}", now.with_timezone(&Local));
+    println!("{:?}", Local.from_utc_datetime(&now.naive_local()));
 
 
-fn createfile(message: String) {
-    let data = message;
-    fs::write("/usr/local/packages/axis_acapwith_rust/vivek.txt", data).expect("Unable to write file");
 
 }
-
-fn main() -> io::Result<()> {
-    createfile("Hi Vivek\n".to_string());
-    Ok(())
-} 
 
 
