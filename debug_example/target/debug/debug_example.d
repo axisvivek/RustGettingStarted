b1689d0ff69d4b44b517c9772a1d88d5d843fdef
Vivek Kumar <axis.vivekk@gmail.com>
2019-12-31 13:09:34 +0530

    Sample2: Debug and display for structure

---
+++ debug_example/target/debug/debug_example.d
@@ -0,0 +1,1 @@
+/mnt/d/Git/Rust/RustProjects/debug_example/target/debug/debug_example: /mnt/d/Git/Rust/RustProjects/debug_example/src/main.rs
---
+++ debug_example/target/debug/deps/debug_example-b7ec832b48f8d65e.d
@@ -0,0 +1,5 @@
+/mnt/d/Git/Rust/RustProjects/debug_example/target/debug/deps/debug_example-b7ec832b48f8d65e: src/main.rs
+
+/mnt/d/Git/Rust/RustProjects/debug_example/target/debug/deps/debug_example-b7ec832b48f8d65e.d: src/main.rs
+
+src/main.rs:
