d9849bd808567539aea545051bd0eb820ce10a81
Vivek Kumar <axis.vivekk@gmail.com>
2019-12-31 11:38:06 +0530

    Sample2: Write in file

--- sample2/build.sh
+++
@@ -1,3 +0,0 @@
-cargo build --target=armv7-unknown-linux-gnueabihf
-
-cp target/armv7-unknown-linux-gnueabihf/debug/sample2 /mnt/d/Git/Rust/AxisACAPRust
--- sample2/src/main.rs
+++ sample2/src/main.rs
@@ -1,26 +1,15 @@
-use std::net::{TcpListener, TcpStream};
 use std::io;
 use std::fs;
 
-fn handle_client(_stream: TcpStream) {
-    // ...
-   createfile();
-}
 
-fn createfile() {
-    let data = "Some data!";
+fn createfile(message: String) {
+    let data = message;
     fs::write("/usr/local/packages/axis_acapwith_rust/vivek.txt", data).expect("Unable to write file");
 
 }
 
 fn main() -> io::Result<()> {
-    createfile();
-    let listener = TcpListener::bind("192.168.1.115:8089")?;
-
-    // accept connections and process them serially
-    for _stream in listener.incoming() {
-        handle_client(_stream?);
-    }
+    createfile("Hi Vivek\n".to_string());
     Ok(())
 } 
 
